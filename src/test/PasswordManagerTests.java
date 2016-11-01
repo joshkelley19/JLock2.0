5c07a3e911147256070cd544bd90ecd244df9faf
joshuakelley <joshkelley19@gmail.com>
2016-11-01 14:17:45 -0400

    added tests

---
+++ src/test/PasswordManagerTests.java
@@ -0,0 +1,18 @@
+import kelley.josh.controller.PasswordManager;
+import org.junit.Before;
+
+/**
+ * Created by joshuakelley on 11/1/16.
+ */
+public class PasswordManagerTests {
+    PasswordManager passwordManager;
+
+    @Before
+    public void initialize(){
+        passwordManager = new PasswordManager();
+    }
+
+    public void customizeTest0(){
+        int[] specs = {1,2,3};
+    }
+}
