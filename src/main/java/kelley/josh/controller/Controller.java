e745e130c21357eeabea565fe2449f9814b92928
joshuakelley <joshkelley19@gmail.com>
2016-11-01 14:08:53 -0400

    added Controller

--- src/main/java/kelley/josh/controller/Controller.java
+++ src/main/java/kelley/josh/controller/Controller.java
@@ -1,69 +1,50 @@
 package kelley.josh.controller;
 
-import Model.EntryManager;
 import kelley.josh.domain.JLockEntry;
-import kelley.josh.domain.User;
-import Model.ProfileManager;
+import kelley.josh.repositories.JLockEntryRepository;
+import kelley.josh.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by joshuakelley on 9/22/16.
  */
+@RestController("/")
 public class Controller {
-    EntryManager entryManager = new EntryManager();
-    JLockEntry currentEntry;
-    ProfileManager profileManager = new ProfileManager();
-    User currentUser;
+    @Autowired
+    UserRepository userRepository;
 
-    public boolean welcome(int selection){
-        switch (selection){
-            case 1:break;//signup return boolean
-            case 2:break;//signin return boolean
-            case 3:return false;//exit
-            default:return true;
-        }
-        return true;
+    @Autowired
+    JLockEntryRepository jLockEntryRepository;
+
+    @RequestMapping(value = "entries", method = RequestMethod.GET)
+    public List<JLockEntry> getEntries(Long id){
+        return jLockEntryRepository.findById(id);
     }
 
-    public boolean landingPage(int selection){
-        switch (selection){
-            case 1:break;//view entries
-            case 2:break;//edit profile
-            case 3:break;//
-        }
-        return true;
+    @RequestMapping(value = "delete", method = RequestMethod.DELETE)
+    public JLockEntry deleteEntry(Long id){
+        JLockEntry deletedEntry = jLockEntryRepository.findOne(id);
+        jLockEntryRepository.delete(deletedEntry);
+        return deletedEntry;
     }
 
-    public boolean manageEntry(int selection){
-        switch (selection){
-            case 1:break;//addProfile
-            case 2:break;//edit
-            case 3:break;//delete
-        }
-        return true;
+    @RequestMapping(value = "entries", method = RequestMethod.PUT)
+    public JLockEntry updateEntry(@RequestBody JLockEntry entry){
+        return jLockEntryRepository.saveAndFlush(entry);//update?
     }
 
-    public void setSelectedEntry(int entryIndex){
-        currentEntry=entryManager.entries.get(entryIndex);
+    @RequestMapping(value = "entries", method = RequestMethod.POST)
+    public JLockEntry createEntry(JLockEntry jLockEntry){
+        return jLockEntryRepository.save(jLockEntry);
     }
 
-    /**
-     * searches accounts to match user. if user name exists, compares entered password
-     * to password connected to user name.
-     * @param userName
-     * @param password
-     * @return boolean
-     */
-    public boolean searchAccounts(String userName,String password){
-        for (String u: profileManager.profiles.keySet()){
-            if(u.equals(userName)){
-                if (profileManager.profiles.get(u).getPassword().equals(password))return true;
-            }
-        }
-        return false;
+    @RequestMapping(value = "entries", method = RequestMethod.POST)
+    public String createCustomEntry(Object[] specs){
+        return PasswordManager.createCustomSpecifications(specs);
     }
 
-    public void setCurrentUser(User verifiedUser){
-        this.currentUser = verifiedUser;
-    }
 
 }
---
+++ src/main/java/kelley/josh/controller/PasswordManager.java
@@ -0,0 +1,11 @@
+package kelley.josh.controller;
+
+/**
+ * Created by joshuakelley on 11/1/16.
+ */
+public class PasswordManager {
+    static String createCustomSpecifications(Object[] specs){
+
+        return "";
+    }
+}
